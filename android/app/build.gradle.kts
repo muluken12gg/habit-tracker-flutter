505ab3bdfffa1dc7b9ebc69ab7013b860503780b
Muluken Tsegaye <muluken.1.3.b@gmail.com>
2026-07-15 16:46:35 -0700

    fix: resolve gradle and android sdk issues for apk build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.habittracker.app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
